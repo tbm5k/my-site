f0636a13b5f7b2d0aada12e3fb763bb18d02cce0
TBM5K <tedburg5@gmail.com>
2020-03-12 00:01:30 +0300

    adding a contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,12 +1,38 @@
 import React from 'react'
 
-function Contact(){
-    return(
-        <div>
-            <h2>Contact</h2>
-            {console.log('changed to Contact page')}
-        </div>
-    );
+class Contact extends React.Component{
+
+    constructor(props){
+        super(props);
+        this.state = {
+            value : ' '
+        }
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event){
+        this.setState({
+            value: event.target.value
+        });
+    }
+
+    handleSubmit(event){
+        alert('Name : ' + this.state.value);
+        event.preventDefault();
+    }
+
+    render(){
+        return(
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <input type="text" value={this.state.value} onChange={this.handleChange}/>
+                    <input type="submit" value="submit"/>
+                </form>
+            </div>
+        );
+    }
 }
 
 export default Contact
